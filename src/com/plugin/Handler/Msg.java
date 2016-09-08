486e381ebb6ccb4ddc64970d258cccaa2bb524ba
FelixVogel <entelp2803@gmail.com>
2016-09-08 13:18:19 +0200

    Build 00a03
    Build 00a04

--- src/com/plugin/Handler/Msg.java
+++ src/com/plugin/Handler/Msg.java
@@ -5,16 +5,16 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import com.coder.client.main.YmlFile;
 
 public class Msg {
-	
+
 	private static YamlConfiguration c;
-	
+
 	public Msg() {
 		c = YmlFile.loadConfig("messages.yml");
 	}
-	
+
 	public static String getMSG(String string) {
 		return c.getString(string);
-		
+
 	}
-	
+
 }
--- src/com/plugin/commands/Gamemode.java
+++ src/com/plugin/commands/Gamemode.java
@@ -13,64 +13,61 @@ public class Gamemode extends CoderCommand {
 		setPermission("");
 	}
 
-	public boolean onCmd(CommandSender arg0, String[] args)
-  {
-    Player sender = (Player)arg0;
-    if (((sender.hasPermission("minka.gm.self")) || (sender.isOp())) && 
-      (args.length == 1)) {
-      if (args[0].equals("0")) {
-        sender.setGameMode(GameMode.SURVIVAL);
-        msg(sender, "&bDu bist jezt im SURVIVAL modus");
-        return true;
-      }
-      if (args[0].equals("1")) {
-        sender.setGameMode(GameMode.CREATIVE);
-        msg(sender, "&bDu bist jezt im CREATIVE modus");
-        return true;
-      }
-      if (args[0].equals("2")) {
-        sender.setGameMode(GameMode.ADVENTURE);
-        msg(sender, "&bDu bist jezt im ADVENTURE modus");
+	public boolean onCmd(CommandSender arg0, String[] args) {
+		Player sender = (Player)arg0;
+		if(((sender.hasPermission("minka.gm.self"))||(sender.isOp()))&&(args.length==1)) {
+			if(args[0].equals("0")) {
+				sender.setGameMode(GameMode.SURVIVAL);
+				msg(sender, "&bDu bist jezt im SURVIVAL modus");
+				return true;
+			}
+			if(args[0].equals("1")) {
+				sender.setGameMode(GameMode.CREATIVE);
+				msg(sender, "&bDu bist jezt im CREATIVE modus");
+				return true;
+			}
+			if(args[0].equals("2")) {
+				sender.setGameMode(GameMode.ADVENTURE);
+				msg(sender, "&bDu bist jezt im ADVENTURE modus");
 
-        return true;
-      }
-      if (args[0].equals("3")) {
-        sender.setGameMode(GameMode.SPECTATOR);
-        msg(sender, "&bDu bist jezt im SPECTATOR modus");
-        return true;
-      }
+				return true;
+			}
+			if(args[0].equals("3")) {
+				sender.setGameMode(GameMode.SPECTATOR);
+				msg(sender, "&bDu bist jezt im SPECTATOR modus");
+				return true;
+			}
 
-    }
-    
-    if ((args.length == 2) && 
-    	      (isOnline(args[0])) && (
-    	      (sender.hasPermission("minka.gm.other")) || (sender.isOp()))) {
-    	
-    Player p = getOnlinePlayer(args[0]);
-    		
-    if (args[1].equals("0")) {
-        p.setGameMode(GameMode.SURVIVAL);
-        msg(p, "&bDu bist jezt im SURVIVAL modus");
-        return true;
-      }
-      if (args[1].equals("1")) {
-        p.setGameMode(GameMode.CREATIVE);
-        msg(p, "&bDu bist jezt im CREATIVE modus");
-        return true;
-      }
-      if (args[1].equals("2")) {
-        p.setGameMode(GameMode.ADVENTURE);
-        msg(p, "&bDu bist jezt im ADVENTURE modus");
+		}
 
-        return true;
-      }
-      if (args[1].equals("3")) {
-        p.setGameMode(GameMode.SPECTATOR);
-        msg(p, "&bDu bist jezt im SPECTATOR modus");
-        return true;
-      }
+		if((args.length==2)&&(isOnline(args[0]))&&((sender.hasPermission("minka.gm.other"))||(sender.isOp()))) {
 
-    }
+			Player p = getOnlinePlayer(args[0]);
 
-	return false;
-}}
+			if(args[1].equals("0")) {
+				p.setGameMode(GameMode.SURVIVAL);
+				msg(p, "&bDu bist jezt im SURVIVAL modus");
+				return true;
+			}
+			if(args[1].equals("1")) {
+				p.setGameMode(GameMode.CREATIVE);
+				msg(p, "&bDu bist jezt im CREATIVE modus");
+				return true;
+			}
+			if(args[1].equals("2")) {
+				p.setGameMode(GameMode.ADVENTURE);
+				msg(p, "&bDu bist jezt im ADVENTURE modus");
+
+				return true;
+			}
+			if(args[1].equals("3")) {
+				p.setGameMode(GameMode.SPECTATOR);
+				msg(p, "&bDu bist jezt im SPECTATOR modus");
+				return true;
+			}
+
+		}
+
+		return false;
+	}
+}
